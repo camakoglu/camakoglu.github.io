29b6052958ff5db121e62e58fc99ddbf8c945b53
camakoglu <camakoglu@gmail.com>
2025-01-01 20:34:22 -0500

    init

---
+++ js/dark_mode.js
@@ -0,0 +1,4 @@
+Array.from(document.querySelectorAll("style")).forEach((sheet) => {
+	if (sheet.textContent?.includes("-webkit-filter: hue-rotate(180deg) invert(100%) !important;"))
+		sheet.parentNode?.removeChild(sheet);
+});
